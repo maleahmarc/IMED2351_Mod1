7de78c310928f78b8929eefdd44e78f281bd798f
kaitlynmarcatante <kaitlynmarcatante@gmail.com>
2026-08-28 13:26:39 -0500

    Update1

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log("Do you like perfume, and if so, do you have a favorite?");
+document.getElementById("Placeholder").innerHTML =
+  "I used the document.getElementById because I read that is the updated version of document.write";
